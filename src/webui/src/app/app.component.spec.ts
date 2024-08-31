c0aa51ea31320745b50fa4f8905b36b5a9f0d163
htc-kymk <haticekaymak476@gmail.com>
2024-09-01 01:16:56 +0300

    .

--- src/webui/src/app/app.component.spec.ts
+++ src/webui/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'issue-management' title`, () => {
+  it(`should have the 'issue' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('issue-management');
+    expect(app.title).toEqual('issue');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, issue-management');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, issue');
   });
 });
